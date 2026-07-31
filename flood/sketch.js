7f19ea60a4600d8d3b5b49e3913519119afab78b
Mimi Yin <mimiyin@gmail.com>
2026-07-31 14:24:28 -0400

    Checking in version from show.

--- flood/sketch.js
+++ flood/sketch.js
@@ -1,22 +1,28 @@
+let mode = 0;
+let byspeeds = [0.5, 1, 5, 10, 20];
+
 let by = 200;
-let byspeed = 0.1;
+let byspeed = byspeeds[mode];
 let rh;
 let rows = 5;
+let vis_rows = 5;
 let a = 0;
-let go = true;
+let go = false;
 
 let multi = true;
+let reverse = false;
 
 function setup() {
-  createCanvas(2950, windowHeight);
+  createCanvas(windowWidth, windowHeight);
   noCursor();
-  rh = round(height / rows);
+  rh = round(height / vis_rows);
   noStroke();
   background(0);
 }
 
 function draw() {
   background(0);
+  if (reverse) scale(1, -1);
   if (go) by += byspeed;
 
   a++;
@@ -28,7 +34,7 @@ function draw() {
       let _by = by - (rh * row);
       let end = _by - (rh * 0.5);
       for (let y = _by; y >= end; y--) {
-        _a -= 1.5;
+        _a -= 2.5;
         if (_a < 0) _a = 64;
         stroke(255, round(_a));
         line(-10, y, REAL_WIDTH, y);
@@ -60,9 +66,17 @@ function keyPressed() {
       multi = !multi;
       console.log(multi ? 'multi' : 'no multi');
       break;
+    case 'r':
+      reverse = !reverse;
+      by = reverse ? -height : 0;
+      break;
   }
 
   switch (keyCode) {
+    case SHIFT:
+      mode++;
+      mode%=byspeeds.length;
+      byspeed = byspeeds[mode];
     case RIGHT_ARROW:
       byspeed += 0.001;
       break;
